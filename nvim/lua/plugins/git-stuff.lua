ed807f88e73212f5f387c86a0fe0dc7652f9ee74
Hakan Bjorklund <hakan.bjorklund@tele2.com>
2024-04-09 08:54:44 +0200

    Removed keybinds
    Removed gitsigns keybinds for debugging purposes.

--- nvim/lua/plugins/git-stuff.lua
+++ nvim/lua/plugins/git-stuff.lua
@@ -5,7 +5,5 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup()
-
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR><CR>", {})
   end
 }
